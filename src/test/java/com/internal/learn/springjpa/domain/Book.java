341a27e9bf631e3f34d78cd764cf770aa997d538
HarendraSahani <harendra.sahani@gmail.com>
2022-11-15 13:41:05 -0600

    adding the book entity class

--- src/test/java/com/internal/learn/springjpa/domain/Book.java
+++ src/test/java/com/internal/learn/springjpa/domain/Book.java
@@ -1,5 +1,43 @@
 package com.internal.learn.springjpa.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    private String title;
+    private String author;
+    private String isbn;
+    public Book() {
+    }
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
 
 }
